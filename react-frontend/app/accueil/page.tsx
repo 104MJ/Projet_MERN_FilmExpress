e1c4cb8ed30f294d26abf5b233c8f45380174561
Aya Sghaier <aya.sghaier@layxo.com>
2026-02-20 13:05:48 +0100

    v03

---
+++ react-frontend/app/accueil/page.tsx
@@ -0,0 +1,13 @@
+const accueilPage = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur FilmExpress</h1>
+      <p>
+        Découvrez notre collection de films et profitez d'une expérience de
+        streaming exceptionnelle.
+      </p>
+    </div>
+  );
+};
+
+export default accueilPage;
---
+++ react-frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import accueilPage from "./components/AccueilPage";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur FilmExpress</h1>
+      <accueilPage></accueilPage>
+    </div>
+  );
+}
